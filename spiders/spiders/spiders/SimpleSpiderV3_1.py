f77fe6c9466c0f5a85b67a69e16f9988aca856ff
TuxFD <tuxfd@yandex.ru>
2024-08-05 19:24:16 +0300

    output fields' order fix

--- spiders/spiders/spiders/SimpleSpiderV3_1.py
+++ spiders/spiders/spiders/SimpleSpiderV3_1.py
@@ -1,89 +1,106 @@
-import scrapy
-from spiders.items import SpidersItem
-from scrapy.exceptions import CloseSpider
-
-CATEGORIES = [
-    # "Rohrverbindungen/",
-    "Rohrverbindungen/Verschraubungen/",
-    "Rohrverbindungen/Flanschverbindungen/",
-    "Rohrverbindungen/Clampverbindungen/",
-    "Rohrverbindungen/Schlauchverbindungen/",
-    "Rohrverbindungen/Industriefittings/",
-]
-
 # ===========================================================
 # NOTE: start_urls обязательно содержит минимум 1 ссылку
 # TODO: понять почему при нескольких ссылках start_urls запись в output.csv ломается
 # TODO: как гонять паука по нескольким категориям сайта? (вытекает из предыдущего)
-# TODO: Строка 47. Заменить счётчик на scrapy + selenium чтобы сразу получить количество страниц товаров
 # ===========================================================
 
 
+import re
+import scrapy
+# from scrapy.exceptions import CloseSpider
+
+CATEGORIES = []
+DOMAIN = "https://shop.ms-armaturen.de/"
+SORTING = "?order=m-s-artikelnummer-aufsteigend&p="
+
+
 class SimpleSpiderV3(scrapy.Spider):
-    name = "SimpleSpiderV3_1"
+    name = "SimpleSpiderV3_2"
     allowed_domains = ["shop.ms-armaturen.de"]
     start_urls = [
-        "https://shop.ms-armaturen.de/Rohrverbindungen/Verschraubungen/?order=m-s-artikelnummer-aufsteigend&p=1",
+        # "https://shop.ms-armaturen.de/Rohrverbindungen/Verschraubungen/?order=m-s-artikelnummer-aufsteigend&p=1",
+        # "https://shop.ms-armaturen.de/Hygienearmaturen/T-Stuecke/?order=m-s-artikelnummer-aufsteigend&p=2",
+        "https://shop.ms-armaturen.de/Zubehoer/Behaelterarmaturen/?order=m-s-artikelnummer-aufsteigend&p=4",
     ]
+    custom_settings = {
+        # "CONCURRENT_REQUESTS": 2,
+        "FEED_EXPORT_FIELDS": [
+            "URL страницы",
+            "Заголовок",
+            "Артикул/SKU",
+            "Цена",
+            "Наличие",
+            "Вес",
+            "Картинки",
+            "Категории",
+        ],
+    }
     visited_urls = []
-    category_url = CATEGORIES[0]
-    current_product_category = ""
+    current_pr_category = ""
 
     def parse(self, response):
-        # учёт посещённых ссылок
         if response.url not in self.visited_urls:
             self.visited_urls.append(response.url)
-            self.current_product_category = self.category_url.replace("/", ", ")
 
             products_table = False
             products_table = response.xpath("//tbody").extract()
-            # page404 = False
-            # page404 = response.xpath(
-            #     "//h1[contains(text(), 'Seite nicht gefunden')]"
-            # ).extract()
 
             # парсим страницы товаров при наличии
             if products_table:
+                self.current_pr_category = self.get_category(str(response.url))
                 for product_link in response.xpath("//tbody/tr/td/a/@href").extract():
                     yield response.follow(
                         product_link,
                         callback=self.parse_product,
                     )
 
-                # переходим на следующие страницы внутри категории
-                for i in range(2, 35):
-                    curr_url = str(response.url)
-                    next_url = curr_url.replace("&p=1", "&p=" + str(i))
-                    yield response.follow(next_url, callback=self.parse)
+            # # переходим на следующие страницы внутри категории
+            # for i in range(2, 35):
+            #     curr_url = str(response.url)
+            #     next_url = curr_url.replace("&p=1", "&p=" + str(i))
+            #     yield response.follow(next_url, callback=self.parse)
 
             # нет товаров - останавливаем паука
             # else:
             #     raise CloseSpider("End of products table!")
 
+    def get_category(self, string):
+        string1 = string.replace(DOMAIN, "").replace(SORTING, "").replace("/", ", ")
+        string2 = re.sub(r"\d", "", string1)
+        return string2
+
     def parse_product(self, response):
-        item = SpidersItem()
         pr_url = response.url
         pr_header = response.xpath(
             "//h1[@class='product-detail-name']/text()"
         ).extract()
-        pr_articul = response.xpath(
-            "//span[@class='product-detail-ordernumber']/text()"
-        ).extract()
-        pr_price = response.xpath("//p[@class='product-detail-price']/text()").extract()
+        pr_articul = (
+            response.xpath("//span[@class='product-detail-ordernumber']/text()")
+            .extract()
+        )
+        pr_price = (
+            response.xpath("//p[@class='product-detail-price']/text()")
+            .extract()
+        )
         pr_stock = response.xpath(
             "//div[@class='product-detail-delivery-information']//p/text()"
         ).extract()
-        pr_weight = response.xpath(
-            "//span[@class='twt-product-detail-weight']/text()"
+        pr_weight = (
+            response.xpath("//span[@class='twt-product-detail-weight']/text()")
+            .extract()
+        )
+        pr_pictures = response.xpath(
+            "//div[@class='gallery-slider-thumbnails-item-inner']/img/@src"
         ).extract()
-        pr_categories = self.current_product_category
+        pr_categories = self.current_pr_category
 
-        item["pr_url"] = pr_url
-        item["pr_header"] = pr_header
-        item["pr_articul"] = pr_articul
-        item["pr_price"] = pr_price
-        item["pr_stock"] = pr_stock
-        item["pr_weight"] = pr_weight
-        item["pr_categories"] = pr_categories
-
-        yield item
+        yield {
+            "URL страницы": pr_url,
+            "Заголовок": pr_header,
+            "Артикул/SKU": pr_articul,
+            "Цена": pr_price,
+            "Наличие": pr_stock,
+            "Вес": pr_weight,
+            "Картинки": pr_pictures,
+            "Категории": pr_categories,
+        }
