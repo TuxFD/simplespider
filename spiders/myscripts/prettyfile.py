a92dac04d4e45548dc8f308752362e7f8353ff29
TuxFD <tuxfd@yandex.ru>
2024-08-06 22:26:46 +0300

    helpers_update

--- spiders/myscripts/prettyfile.py
+++ spiders/myscripts/prettyfile.py
@@ -1,37 +1,50 @@
-DEFAULT_FILENAME = "../spiders/files/raw/output.csv"
-IN_STOCK_FALSE = "Bitte wenden Sie sich an Ihren zuständigen M&S Vertriebsmitarbeiter"
-IN_STOCK_TRUE = "Auf Lager."
+from myconf.config import (
+    DEFAULT_FILENAME_PRETTYFILE,
+    HEADERS,
+    FILENAME_SUMMARY,
+    IN_STOCK_TRUE,
+    IN_STOCK_FALSE,
+)
+
 
 def prettyfile():
-    filename_raw = DEFAULT_FILENAME
-    filename_cooked = DEFAULT_FILENAME.replace("/raw/", "/cooked/")
+    filename_cooked = FILENAME_SUMMARY
+    f = open(filename_cooked, "w+")
+    f.close()
 
-    lines = []
     no_headers = True
 
-    with open(filename_raw, "r") as fr:
-        lines = fr.readlines()
+    for filename_raw in DEFAULT_FILENAME_PRETTYFILE:
 
-    with open(filename_cooked, "w+") as fc:
-        for line in lines:
+        lines = []
+        headers = True
 
+        with open(filename_raw, "r") as fr:
+            lines = fr.readlines()
+
+        with open(filename_cooked, "a") as fc:
             if no_headers:
-                fc.write(line)
+                fc.write(HEADERS)
                 no_headers = False
-                continue
 
-            newline = (
-                line.replace("  ", "")
-                .replace("Katalogpreis: ", "")
-                .replace(",https", " ,https")
-                .replace(',"\n', ',"')
-                .replace("Derzeit nicht auf Lager.,\n", "")
-                .replace(IN_STOCK_TRUE, "Да")
-                .replace(IN_STOCK_FALSE, 'Нет')
-            )
-            fc.write(newline)
+            for line in lines:
 
-    print("Success. File prettified: ", filename_cooked)
+                if headers:
+                    headers = False
+                    continue
+
+                newline = (
+                    line.replace("  ", "")
+                    .replace("Katalogpreis: ", "")
+                    .replace(",https", " ,https")
+                    .replace(',"\n', ',"')
+                    .replace("Derzeit nicht auf Lager.,\n", "")
+                    .replace(IN_STOCK_TRUE, "Да")
+                    .replace(IN_STOCK_FALSE, "Нет")
+                )
+                fc.write(newline)
+
+        print("Success. File prettified: ", filename_cooked)
 
 
 if __name__ == "__main__":
--- spiders/myscripts/urls_generator.py
+++ spiders/myscripts/urls_generator.py
@@ -1,25 +1,12 @@
-DOMAIN = "https://shop.ms-armaturen.de/"
-SORTING = "?order=m-s-artikelnummer-aufsteigend&p="
-
-CATEGORIES = [
-    # "Rohrverbindungen/",
-    "Rohrverbindungen/Verschraubungen/",
-    "Rohrverbindungen/Flanschverbindungen/",
-    "Rohrverbindungen/Clampverbindungen/",
-    "Rohrverbindungen/Schlauchverbindungen/",
-    "Rohrverbindungen/Industriefittings/",
-]
-
-
-start_urls = []
+from myconf.config import DOMAIN, SORTING, CATEGORIES
 
 
 def urls_generator():
+    start_urls = []
     for category in CATEGORIES:
         start_urls.append(DOMAIN + category + SORTING + "1")
     for url in start_urls:
         print(url)
-    ...
 
 
 if __name__ == "__main__":
